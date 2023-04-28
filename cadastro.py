337a946d0e2e247333bae21464d3dd2d16a84d84
RianYuri <rianyurirgt@gmail.com>
2023-04-28 19:47:38 -0300

    feat: next page

--- cadastro.py
+++ cadastro.py
@@ -7,7 +7,6 @@ tela.geometry("1280x720")
 tela.resizable(True, True)
 tela.maxsize(width=1280, height=720)
 tela.minsize(width=1280, height=720)
-tela.configure(background="#FFFFFF")
 
 dogImg = Image.open("./img/dogImage.png")
 dogImage = ImageTk.PhotoImage(dogImg)
@@ -15,12 +14,22 @@ dogImage = ImageTk.PhotoImage(dogImg)
 dogImg = tk.Label(tela,image=dogImage )
 dogImg.grid(row=0, column=1, rowspan=2, sticky="e",padx=110,pady=10)
 
+label_nameDog = Label(tela, text="Nome do seu pet")
+label_nameDog.grid(row=0,column=0, sticky="s")
+
+
+IdadeDog = Entry(tela, width=29)
+IdadeDog.grid(row=1, column=0 ,rowspan=2,  sticky="n"  )
 nameDog = Entry(tela, width=29)
-nameDog.grid(row=0, column=0   )
+nameDog.grid(row=1, column=0 ,sticky="ne"  )
 
 tela.grid_rowconfigure(0, weight=1)
 tela.grid_rowconfigure(1, weight=1)
+tela.grid_rowconfigure(2, weight=1)
+tela.grid_rowconfigure(3, weight=1)
 tela.grid_columnconfigure(0, weight=1)
 tela.grid_columnconfigure(1, weight=1)
+tela.grid_columnconfigure(2, weight=1)
+tela.grid_columnconfigure(3, weight=1)
 tela.mainloop()
 
--- home.py
+++ home.py
@@ -2,7 +2,7 @@ from tkinter import *
 import tkinter as tk
 from PIL import Image, ImageTk
 import tkinter.font as font 
-
+import subprocess
 tela = tk.Tk()
 tela.geometry("1280x720")
 tela.resizable(True, True)
@@ -13,10 +13,17 @@ tela.configure(background="#FFFFFF")
 caminho_fonte = "./fonts/Londrina_Shadow/LondrinaShadow-Regular.ttf"
 
 
-fontePetshop = tk.font.Font(family="Londrina Shadow", size=84)
+fontePetshop = tk.font.Font(family="Londrina Shadow", size=70)
 texto_label = tk.Label(tela, text="PetShop Dog’s", font=fontePetshop)
 texto_label.configure(background='white')
 texto_label.grid(row=0, column=0, rowspan=2,columnspan=2, sticky="nw", pady=181, padx=61)
+
+
+def nextPage():
+    subprocess.run(["py","cadastro.py"])
+
+getMore = Button(tela, width=30 , text="Entrar", bg="orange", bd=0, font="roboto", command=nextPage)
+getMore.place(x=100,y=500)
  
 topImge = Image.open("./img/topImage.png")
 imagem_tk = ImageTk.PhotoImage(topImge)
@@ -26,14 +33,16 @@ dogImge = Image.open("./img/dogImage.png")
 dogImage = ImageTk.PhotoImage(dogImge)
 dogImg = tk.Label(tela,image=dogImage )
 
+
 bottomImge = Image.open("./img/bottomImg.png")
 dog_bottom = ImageTk.PhotoImage(bottomImge)
 bottomImageDog = tk.Label(tela, image=dog_bottom)
-
+dogImg.configure(background='white')
 dogImg.grid(row=0, column=1, rowspan=2, sticky="e",padx=110,pady=10)
 topImageDog.grid(row=0, column=0, columnspan=2, sticky="n")
+topImageDog.configure(background='white')
 bottomImageDog.grid(row=1, column=1, sticky="se", padx=0, pady=10)
-
+bottomImageDog.configure(background='white')
 tela.grid_rowconfigure(0, weight=1)
 tela.grid_rowconfigure(1, weight=1)
 tela.grid_columnconfigure(0, weight=1)
